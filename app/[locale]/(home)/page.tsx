c0101b33e52ca0995464971a3594b8170f9b3630
Mac <mac@abs-MacBook-Pro.local>
2024-01-20 18:11:48 +0600

    testing

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -1,14 +1,29 @@
+"use client";
+
 import { CategoryNav } from "@/app/layouts/category-nav";
 import { HeroSection } from "@/app/[locale]/(home)/components/hero-section";
 import { useTranslations } from "next-intl";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 export default function Home() {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/app/ui/editor"), { ssr: false }),
+    []
+  );
   const t = useTranslations();
+
+  const onChange = (content: string) => {
+    console.log(content);
+  };
+
   return (
     <div className="h-[200vh]">
       {/* <div className="container mt-[10px] !sticky !top-[10px]">
         <CategoryNav />
       </div> */}
+
+      <Editor onChange={onChange} />
 
       <HeroSection />
     </div>
